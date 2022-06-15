5ba9b1f501df0c59115b31fd1bed47f4ce2ed6d0
michellejanosi <michelle.janosi@gmail.com>
2022-06-15 05:54:08 -0400

    Reset count to 0

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ let count = 0;
 
 function increment() {
   count += 1;
-  return countEl.textContent = count;
+  countEl.textContent = count;
 }
 
 function save() {
   let saveCount = `${count} - `;
   saveEl.textContent += saveCount;
+  // rest countEl to 0 after save
+  count = 0;
+  countEl.textContent = 0;
 }
