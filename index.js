64e8d86b7443f913721b4887e8d7d025f50c0f91
michellejanosi <michelle.janosi@gmail.com>
2022-06-15 05:40:29 -0400

    Debug and refactor previous entries

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ let count = 0;
 
 function increment() {
   count += 1;
-  return countEl.innerText = count;
+  return countEl.textContent = count;
 }
 
 function save() {
-  let saveCount = `${count} -`;
-  saveEl.innerText += saveCount;
+  let saveCount = `${count} - `;
+  saveEl.textContent += saveCount;
 }
